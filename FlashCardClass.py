3e4f7c11a09fcc0384a0add9ed6406816ffa89d5
seandye13 <123012514+seandye13@users.noreply.github.com>
2023-04-17 10:45:31 -0700

    git commit

--- FlashCardClass.py
+++ FlashCardClass.py
@@ -1,4 +1,5 @@
 from graphics import *
+from Button import Button
 
 class GraphicInterface:
     
@@ -6,19 +7,25 @@ class GraphicInterface:
         self.win = GraphWin('flashcard', 500, 500)
         self.win.setCoords(0, 0, 100, 100)
         self.win.setBackground('light blue')
-        
-    def drawCard(self, text):
+        self.backButton = Button(self.win, Point(30, 10), 36, 10, 'Back')
+        self.awnserButton = Button(self.win, Point(70, 10), 36, 10, 'Awnser')
+    
+    def drawCard(self):
         rect = Rectangle(Point(15, 30), Point(85, 70))
         rect.setFill('white')
         rect.draw(self.win)
-        quest = Text(Point(50, 50), 'What was the release date of\n'
-                     'the first Scream movie')
-        quest.draw(self.win)
     
+    def text(self):
+        self.text = Text(Point(50, 50), 'What was the release date of\n'
+                     'the first Scream movie')
+        self.text.draw(self.win)
+        
     def run(self):
         self.drawCard()
+        self.text()
         self.win.getMouse()
         self.win.close()
     
 if __name__ == "__main__":
-    GraphicInterface().run()
+    app = GraphicInterface()
+    app.run()
